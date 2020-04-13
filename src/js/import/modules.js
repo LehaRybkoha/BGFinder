8e5c499a2f31b0aa104ae24be13814070bf60ed6
LehaRybkoha <emailforwork134@gmail.com>
2020-04-13 03:59:46 +0300

    Finished main page

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,8 @@
 import "%modules%/header/header";
 import "%modules%/about/about";
 import "%modules%/steps/steps";
+import "%modules%/calc/calc";
+import "%modules%/products/products";
+import "%modules%/company/company";
+import "%modules%/news/news";
 import "%modules%/footer/footer"
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,30 @@
 import Vue from 'vue';
-import VueScreenSize from 'vue-screen-size'
+import VueScreenSize from 'vue-screen-size';
+import VueSlider from 'vue-slider-component';
+import { format, formatDistance, formatRelative, subDays } from './import/date_fns';
+import AirbnbStyleDatepicker from './import/vue-airbnb-style-datepicker';
 import "./import/modules.js";
 $.fn.hasAttr = function(name) {
    return this.attr(name) !== undefined;
 };
-
+var datepickerOptions = {
+  
+}
+Vue.use(AirbnbStyleDatepicker, datepickerOptions)
 Vue.use(VueScreenSize);
 
-new Vue({
+let app = new Vue({
   el: '#wrapper',
+  components: {
+      VueSlider
+  },
   data: {
-    show:false,
+    showNav:false,
+    dateFormat: 'YYYY-MM-D',
+    dateOne: '',
+    dateTwo: '',
+    value: 0,
+    minDate: '',
     navList: [
       { 
         navOption: 'Финансовые продукты',
@@ -45,8 +59,141 @@ new Vue({
         stepsDesc: 'Получите предложение от нескольких банков и выберите максимально комфортное для вас'
       }
     ],
+    calcList: [
+      { 
+        calcText: 'Банковские гарантии по 223–ФЗ и 185–ФЗ',
+      },
+      { 
+        calcText: 'Тендерный займ',
+      },
+      { 
+        calcText: 'Банковские гарантии по 44–ФЗ',
+      },
+      { 
+        calcText: 'Кредиты на исполнение заказа',
+      }
+    ],
+    productsList: [
+      { 
+        productsTitle: 'Банковские гарантии по 223–ФЗ и 185–ФЗ',
+        productsPrice: '700',
+        productsItemListText1: 'Работа со сложными заказчиками',
+        productsItemListText2: 'Подбор банков под конкретную сделку',
+        productsItemListText3: 'Согласование сделок на уровне руководства банка',
+      },
+      { 
+        productsTitle: 'Тендерный займ',
+        productsPrice: '700',
+        productsItemListText1: 'Работа со сложными заказчиками',
+        productsItemListText2: 'Подбор банков под конкретную сделку',
+        productsItemListText3: 'Согласование сделок на уровне руководства банка',
+      },
+      { 
+        productsTitle: 'Банковские гарантии по 44–ФЗ',
+        productsPrice: '900',
+        productsItemListText1: 'Выпуск банковской гарантии за 1–3 дня',
+        productsItemListText2: 'Банковская гарантия без залога, открытия счета и поручительства',
+        productsItemListText3: 'Индивидуальные условия сотрудничества для каждого клиента',
+      },
+      { 
+        productsTitle: 'Кредиты на исполнение заказа',
+        productsPrice: '700',
+        productsItemListText1: 'Работа со сложными заказчиками',
+        productsItemListText2: 'Подбор банков под конкретную сделку',
+        productsItemListText3: 'Согласование сделок на уровне руководства банка',
+      }
+    ],
+    companyList: [
+      { 
+        companyListText: 'Скорость',
+      },
+      { 
+        companyListText: 'Индивидуальный подход',
+      },
+      { 
+        companyListText: 'На связи до выдачи БГ, прямая работа с банками',
+      },
+      { 
+        companyListText: 'Полное сопровождение',
+      },
+      { 
+        companyListText: 'Высокая вероятность добрения БГ',
+      },
+    ],
+    newsList: [
+      { 
+        newsPic: '/img/newsPic1.png',
+        newsText: 'Плюсы и минусы банковских гарантий',
+      },
+      { 
+        newsPic: '/img/newsPic2.png',
+        newsText: 'Банковская гарантия: ее риски при использовании в тендерах',
+      },
+      { 
+        newsPic: '/img/newsPic3.png',
+        newsText: 'Суд на защите банковской гарантии, интересов принципала, гаранта и бенефициара',
+      },
+    ],
+    footerListOne: [
+      { 
+        footerLink: 'Банковские Гарантии 44-ФЗ',
+      },
+      { 
+        footerLink: 'Исполнение Контракта',
+      },
+      { 
+        footerLink: 'Участие в Тендере',
+      },
+      { 
+        footerLink: 'Возврат Аванса',
+      },
+      { 
+        footerLink: 'Гарантийные Обязательства',
+      },
+    ],
+    footerListTwo: [
+      { 
+        footerLink: 'Исполнение Контракта',
+      },
+      { 
+        footerLink: 'Возврат Аванса',
+      },
+      { 
+        footerLink: 'Участие В Торгах',
+      },
+      { 
+        footerLink: 'Возврат Аванса',
+      },
+      { 
+        footerLink: 'Гарантийные Обязательства',
+      },
+    ],
   },
   methods: {
-
-  }
+     formatDates(dateOne, dateTwo) {
+      let formattedDates = ''
+      if (dateOne) {
+        formattedDates = format(dateOne, this.dateFormat)
+      }
+      if (dateTwo) {
+        formattedDates += ' - ' + format(dateTwo, this.dateFormat)
+      }
+      return formattedDates
+    }
+  },
 });
+
+var timerID = setInterval(updateTime, 1000);
+updateTime();
+function updateTime() {
+  var cd = new Date();
+  app.minDate = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ';
+};
+
+function zeroPadding(num, digit) {
+    var zero = '';
+    for(var i = 0; i < digit; i++) {
+        zero += '0';
+    }
+    return (zero + num).slice(-digit);
+}
